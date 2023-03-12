b7b0c8dba2a21c474889d5077f0442dcf022349c
MustaphaBousilDev <bousilmustapha@gmail.com>
2023-03-12 17:58:31 +0100

    finish display questio,

--- quize/src/pages/FinalScreen.js
+++ quize/src/pages/FinalScreen.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import { Box, Button, Typography } from '@mui/material'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { handleAmountChange, handleScoreChange } from '../redux/actions'
 const Score = () => {
+const {score}=useSelector(state=>state)
+const dispatch=useDispatch()
+const navigate=useNavigate() 
+  const handlePlay=()=>{
+    dispatch(handleScoreChange(0))
+    dispatch(handleAmountChange(50))
+    navigate('/')
+  }
   return (
-    <div>FinalScreen</div>
+    <Box mt={30}>
+      <Typography variant='h3' fontWeight='bold' mb={3}>
+        Final Score {score}
+      </Typography>
+      <Button onClick={handlePlay} variant='outlined'>
+        Play Again
+      </Button>
+    </Box>
   )
 }
 
--- quize/src/pages/Questions.js
+++ quize/src/pages/Questions.js
@@ -1,17 +1,42 @@
 import { Box, Button, CircularProgress, Typography } from "@mui/material"
 import HorizontalLabelPositionBelowStepper from "../components/steperComponent"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import useAxios from "../hooks/useAxios"
 import { useSelector } from "react-redux"
+import { useHistory } from "react-router"
+import { useNavigate } from "react-router-dom"
+import LinearProgressWithLabel from "../components/progressBar"
+import { useDispatch } from "react-redux"
+import { decode } from "html-entities"
+import { handleScoreChange } from "../redux/actions"
+
+const getRandom=(max)=>{
+  return Math.floor(Math.random()*Math.floor(max))
+}
 const Questions = () => {
+  /////////////////////////////////////
+  
+
+  
+  //////////////////////////
   const [value,setValue]=useState(1)
   //use useSelector
   const {
     question_category,
      question_difficulty,
      question_type,
-     amount_questions
+     amount_questions,
+     score
   }=useSelector(state=>state)
+
+  //history
+  const navigate=useNavigate()
+  //dispatch
+  const dispatch=useDispatch()
+
+
+
+
   //console.log('hello motheer fucker')
   //console.log(amount_questions,question_category,question_difficulty,question_type)
   let apiUrl=`api.php?amount=${amount_questions}`
@@ -24,30 +49,67 @@ const Questions = () => {
   if(question_type){
       apiUrl.concat(`&type=${question_type}`)
   }
+  
   const {response,loading}=useAxios({url:apiUrl})
+  const [questionIndex,setQuestionIndex]=useState(0)
+  const [options,setOptions]=useState([])
+ 
+  const ps=(1/amount_questions)*100
+  const [progress, setProgress] = useState(ps);
+  //console.log(options)
+  useEffect(() => {
+    if(response?.results.length){
+      const question=response.results[questionIndex]
+      let answers=[...question.incorrect_answers]
+      answers.splice(getRandom(question.incorrect_answers.length),0,question.correct_answer)
+      setOptions(answers)
+      console.log(response)
+    }
+  }, [response,questionIndex]);
  if(loading){
     return (
-      <Box>
+      <Box mt={20}>
         <CircularProgress/>
       </Box>
     )
  }
   
+  
+
+  const handleAnswer=(e)=>{
+    setProgress((prevProgress) => (prevProgress >= 100 ? ps : prevProgress + ps));
+    const question=response.results[questionIndex]
+    if(e.target.textContent===question.correct_answer){
+        dispatch(handleScoreChange(score + 1))
+    }
+    if(questionIndex + 1 < response.results.length){
+      setQuestionIndex(questionIndex + 1)
+    }else{
+      navigate('/score')
+    }
+  }
   return (
+    <>
     <Box>
-      <Typography mb={2} variant='h2'>Questions</Typography>
+      <Typography mb={2} variant='h2'>Questions {questionIndex + 1}</Typography>
       <HorizontalLabelPositionBelowStepper activeStep={value} steps={['','','']} />
-      <Typography mt={5} variant='h4'>This is Questions ? </Typography>
-      <Box mt={2}>
-        <Button variant='contained'>Answer 1</Button>
-      </Box>
-      <Box mt={2}>
-        <Button variant="contained">Answer 2</Button>
-      </Box>
+      <Typography mt={5} variant='h4'>{decode(response.results[questionIndex].question)}</Typography>
+      {
+        options.map((data,id)=>(
+          <Box mt={2} key={id}>
+            <Button onClick={handleAnswer} variant='contained'>
+              {decode(data)}
+            </Button>
+          </Box>
+        ))
+      }
       <Box mt={5}>
-        Score: 2/6
+        Score: {(score)}/{response.results.length}
       </Box>
     </Box>
+
+    <LinearProgressWithLabel value={progress} />
+    </>
   )
 }
 
